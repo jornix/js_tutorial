069d785bf3dc3347b11bd0b3d87bd45cc6f9e5f9
Jorn Pedersen <jornped@gmail.com>
2018-08-29 22:38:41 +0200

    Add greeting(), modified index.html

--- day.js
+++ day.js
@@ -2,3 +2,7 @@ function dayName(date) {
   const daysOfTheWeek = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag" ];
   return daysOfTheWeek[now.getDay()];
 }
+
+function greeting(date){
+  return `I dag er det heldigvis ${dayName(date)}!`
+}
